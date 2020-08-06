428fd33a2786f958a5d91708a6168f3a2bce8fd5
Kato Shinya <yourdream28@gmail.com>
2020-08-06 09:13:24 +0900

    add

---
+++ src/main/java/org/thinkit/formatter/common/Formatter.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.formatter.common;
+
+import lombok.NonNull;
+
+/**
+ * 入力に対する整形処理を行うフォーマッターを抽象化したインターフェースです。 {@link Formatter} インターフェースを実装する具象クラスは
+ * {@link Formatter#format(String)} メソッドを必ず実装してください。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ *
+ * @see #format(String)
+ */
+public interface Formatter {
+
+    /**
+     * 引数として与えられた {@code sequence} に格納された文字列を整形し返却します。
+     *
+     * @param sequence 整形対象の文字列
+     * @return 引数 {@code sequence} の値を基に整形された文字列
+     *
+     * @exception NullPointerException 引数として {@code null} が渡された場合
+     */
+    public String format(@NonNull final String sequence);
+}
