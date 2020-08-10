17feb4e6832b7a597283238f63da2ac16114474b
Kato Shinya <yourdream28@gmail.com>
2020-08-10 17:25:16 +0900

    add

---
+++ src/main/java/org/thinkit/formatter/common/Indenter.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.formatter.common;
+
+/**
+ * インデント処理を抽象化したインターフェースです。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+public interface Indenter {
+
+    /**
+     * インデント数をインクリメントします。
+     * <p>
+     * この {@link Indenter#increment()}
+     * メソッドは自分自身のインスタンスを返却するため、後続処理をメソッドチェーンの形式で行うことができます。
+     *
+     * @return 自分自身のインスタンス
+     */
+    public Indenter incremenet();
+
+    /**
+     * インデント数をデクリメントします。
+     * <p>
+     * この {@link Indenter#decrement()}
+     * メソッドは自分自身のインスタンスを返却するため、後続処理をメソッドチェーンの形式で行うことができます。
+     *
+     * @return 自分自身のインスタンス
+     */
+    public Indenter decrement();
+
+    /**
+     * インデント数に応じた改行コードを生成し返却します。
+     *
+     * @return インデント数に応じた改行コード
+     */
+    public String newline();
+}
