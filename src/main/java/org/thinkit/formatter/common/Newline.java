6dbf32cd5c669ea256e68e47a824f2de47a2d5da
Kato Shinya <yourdream28@gmail.com>
2020-08-11 10:48:58 +0900

    add

---
+++ src/main/java/org/thinkit/formatter/common/Newline.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.formatter.common;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * インデント数に応じた改行を管理するクラスです。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@ToString
+@EqualsAndHashCode
+public final class Newline implements Line {
+
+    /**
+     * 改行コード
+     */
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
+    /**
+     * インデント
+     */
+    private Indentable indent;
+
+    /**
+     * インデント数に応じた改行コードを生成し返却します。
+     *
+     * @return インデント数に応じた改行コード
+     */
+    @Override
+    public String create() {
+
+        final StringBuilder newline = new StringBuilder().append(LINE_SEPARATOR);
+
+        for (int i = 0, indentFactor = indent.getIndentFactor(); i < indentFactor; i++) {
+            newline.append(indent.getIndent());
+        }
+
+        return newline.toString();
+    }
+}
