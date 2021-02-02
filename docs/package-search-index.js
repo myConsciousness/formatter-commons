205a4b62a8c02f4810f27f8a3be8deb0f88ab21a
Kato Shinya <kato.shinya.dev@gmail.com>
2021-02-02 19:01:02 +0900

    add API document

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"org.thinkit.formatter.common"},{"l":"org.thinkit.formatter.common.catalog"},{"l":"org.thinkit.formatter.common.exception"},{"l":"すべてのパッケージ","u":"allpackages-index.html"}];updateSearchResults();
